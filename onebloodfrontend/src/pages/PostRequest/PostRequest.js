466f16ec0a6df73884ca64c2cbfcdeffe6d99768
him16112 <99254194+him16112@users.noreply.github.com>
2024-11-20 20:03:37 +0530

    Update PostRequest.js

--- onebloodfrontend/src/pages/PostRequest/PostRequest.js
+++ onebloodfrontend/src/pages/PostRequest/PostRequest.js
@@ -24,7 +24,7 @@ const PostRequest = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('https://one-blood-backend.onrender.com', {
+            const response = await fetch('https://one-blood-backend.onrender.com/postRequest', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
